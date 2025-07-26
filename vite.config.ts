9fbb22e7b95b5b61aaece49196064605ff53e854
Dein Name <deine@email.com>
2025-07-26 02:37:20 +0200

    Update vite.config.ts
    IP war auf localhost und jetzt die hardcoded ip

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,11 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'http://192.168.8.204:3001',
         changeOrigin: true,
       },
       '/photos': {
-        target: 'http://localhost:3001',
+        target: 'http://192.168.8.204:3001',
         changeOrigin: true,
       }
     }
